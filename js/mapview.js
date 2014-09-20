4c981a4b6ac6dddf155c1f861fd1429c73ebc610
ikcede <csh342@nyu.edu>
2014-09-20 18:36:51 -0400

    map fixes

--- js/mapview.js
+++ js/mapview.js
@@ -3,8 +3,11 @@ var map;
 // Ready function
 $(document).ready(function() {
 
+    var pyrmont = new google.maps.LatLng(-33.8665433,151.1956316);
+
 	map  = new google.maps.Map(document.getElementById('map'), {
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		center: pyrmont,
 		zoom: 15
 	}); 
 
